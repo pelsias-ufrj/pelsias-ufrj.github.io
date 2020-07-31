f15b701d95fca9070f7449ed22c0615b368ad4a1
muller <lukasmullerdeoliveira@gmail.com>
2020-07-31 15:48:36 -0300

    bug fix in the backend

--- servers.js
+++ servers.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use(express.static('/public/.'));
+app.use(express.static('./public/.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
