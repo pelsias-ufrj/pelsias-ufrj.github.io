ef32b112728506bc025e9ee301e3b19b389542ff
muller <lukasmullerdeoliveira@gmail.com>
2020-08-01 14:49:21 -0300

    started to add a form in the join us page

--- servers.js
+++ servers.js
@@ -1,10 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('./public/.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/join-us', (req, res) => {
+  console.log(req.body);
+  res.send('cadastrado'); 
+});
 app.get('/teste', (req, res) => res.send('ok'));
 app.listen(8080, () => console.log('Executando...'));
