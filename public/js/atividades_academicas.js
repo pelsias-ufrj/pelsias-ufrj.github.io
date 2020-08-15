c41f3febaae411ccc61f036e5cb6e1f47dd6dfe3
pelsias <pels.ufrj@gmail.com>
2020-08-15 19:08:12 -0300

    Desenvolvi a parte do carrossel

--- public/js/atividades_academicas.js
+++ public/js/atividades_academicas.js
@@ -1,6 +1,26 @@
 $('.carousel').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
+  centerMode: true,
+  dots: true,
+  centerPadding: '80px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
