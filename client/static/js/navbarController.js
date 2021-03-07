7c7215b9841e277b86619a329a2d333410026c68
Pedro Lidio <pedrolidio@poli.ufrj.br>
2021-03-07 14:56:02 -0300

    bug fixes in join-us navbar and members page links

--- client/static/js/navbarController.js
+++ client/static/js/navbarController.js
@@ -12,8 +12,57 @@ window.addEventListener('scroll', function () {
     let youtube = document.getElementById('youtube-icon');
     let windowPosition = window.scrollY > 0;
     
-    
-    if (windowPosition && screen.width > 980)
+    if(!document.querySelector('#join-us-page'))
+    {
+        if (windowPosition && screen.width > 980)
+        {
+            nav.classList.add('scrolling-active');
+            title.classList.remove('text-white');
+            home.classList.remove('text-white');
+            equipe.classList.remove('text-white');
+            equipe.style.opacity = "0.5";
+            coordenacoes.classList.remove('text-white');
+            coordenacoes.style.opacity = "0.5";
+            contato.classList.remove('text-white');
+            blog.classList.remove('text-white');
+            facebook.classList.remove('text-white');
+            insta.classList.remove('text-white');
+            linkedin.classList.remove('text-white');
+            youtube.classList.remove('text-white');
+        }
+        else
+        {
+            nav.classList.remove('scrolling-active');
+            title.classList.add('text-white');
+            home.classList.add('text-white');
+            equipe.classList.add('text-white');
+            equipe.style.opacity = "1";
+            coordenacoes.classList.add('text-white');
+            coordenacoes.style.opacity = "1";
+            contato.classList.add('text-white');
+            blog.classList.add('text-white');
+            facebook.classList.add('text-white');
+            insta.classList.add('text-white');
+            linkedin.classList.add('text-white');
+            youtube.classList.add('text-white');
+        }
+    }
+})
+
+$(document).ready(function() {
+    let nav = document.querySelector('nav');
+    let title = document.getElementById('title');
+    let home = document.getElementById('home');
+    let equipe = document.getElementById('equipe');
+    let coordenacoes = document.getElementById('coordenacoes');
+    let contato = document.getElementById('contato');
+    let blog = document.getElementById('blog');
+    let facebook = document.getElementById('facebook-icon');
+    let insta = document.getElementById('insta-icon');
+    let linkedin = document.getElementById('linkedin-icon');
+    let youtube = document.getElementById('youtube-icon');
+
+    if(document.querySelector('#join-us-page'))
     {
         nav.classList.add('scrolling-active');
         title.classList.remove('text-white');
@@ -29,23 +78,8 @@ window.addEventListener('scroll', function () {
         linkedin.classList.remove('text-white');
         youtube.classList.remove('text-white');
     }
-    else
-    {
-        nav.classList.remove('scrolling-active');
-        title.classList.add('text-white');
-        home.classList.add('text-white');
-        equipe.classList.add('text-white');
-        equipe.style.opacity = "1";
-        coordenacoes.classList.add('text-white');
-        coordenacoes.style.opacity = "1";
-        contato.classList.add('text-white');
-        blog.classList.add('text-white');
-        facebook.classList.add('text-white');
-        insta.classList.add('text-white');
-        linkedin.classList.add('text-white');
-        youtube.classList.add('text-white');
-    }
-})
+});
+
 
 function HoverOpacity (dropdown) {
     if(window.scrollY > 0 && screen.width > 980)
